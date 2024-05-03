d99a0d9f18db5844ee9efec39c15bbd7ff20ebc7
Arsenijs Zimovs <arsenij.zimov@gmail.com>
2024-05-03 19:27:08 +0300

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,20 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        searched_number = target - nums[i]
+        for j in range(i):
+            if nums[j] == searched_number:
+                return [i,j]
+                
+# test
+print(twoSum([3,2,4], 6))
+
+                
+
+
